844625f229e49ce6055bcb24dfdbf89318d11b6c
Andhika Dwi Khalisyahputra <andhikadwi980@gmail.com>
2024-03-14 13:51:08 +0700

    JOBSHEET 5 PRAKTIKUM 2

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -11,4 +11,10 @@ class KategoriModel extends Model
 
     protected $table = 'm_kategori';
     protected $primaryKey = 'kategori_id';
+    protected $fillable = ['kategori_kode', 'kategori_nama'];
+
+    public function barang()
+    {
+        return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
+    }
 }
